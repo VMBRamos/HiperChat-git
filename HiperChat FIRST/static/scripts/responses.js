ae74f3fcdc4c7b980fd6bc19d5dfcf6d4e7cabcc
Vitor Ramos <vitor.ramos@hiper.com.br>
2021-09-14 15:28:19 -0300

    teste git

---
+++ HiperChat FIRST/static/scripts/responses.js
@@ -0,0 +1,14 @@
+function getBotResponse(input) {
+
+
+  //colocando para testar as respostas falta fazer a conexao com o banco de dados 
+    if (input == "Olá") {
+        return "Olá, como posso lhe ajudar?";
+
+    } else {
+        return "Não entendi, poderia reformular por gentileza? :)";
+    }
+}
+
+
+
